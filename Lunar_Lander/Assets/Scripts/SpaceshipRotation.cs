58fe90a98f0cae39ee3dcaf51502f4f81269acd7
unknown <facuwilli@gmail.com>
2022-04-26 15:39:22 -0300

    Added spaceship rotation.

--- Lunar_Lander/Assets/Scripts/SpaceshipRotation.cs
+++ Lunar_Lander/Assets/Scripts/SpaceshipRotation.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 
+
 public class SpaceshipRotation : MonoBehaviour
 {
-    [SerializeField] private float spaceshipRotationValue = 90f;
+    [SerializeField] private float spaceshipRotationValue = 5f;
 
-    private Rigidbody rigidBody;   
+    private Rigidbody rb;
 
+    Vector3 rotation;
+        
     private void Awake()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }  
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        RotateSpaceship();        
+        RotateSpaceship();
     }
 
     void RotateSpaceship() 
     {
-        float xRotation = Input.GetAxisRaw("Vertical") * Time.deltaTime * spaceshipRotationValue;
-        float zRotation = Input.GetAxisRaw("Horizontal") * Time.deltaTime * spaceshipRotationValue;
-        
-        rigidBody.MoveRotation(Quaternion.Euler(transform.rotation.eulerAngles.x + xRotation, 0f, transform.rotation.eulerAngles.z + zRotation));        
-    }
+        rotation.x = Input.GetAxisRaw("Vertical") * Time.deltaTime * spaceshipRotationValue;
+        rotation.z = Input.GetAxisRaw("Horizontal") * Time.deltaTime * spaceshipRotationValue;
+
+        rb.AddTorque(rotation);
+
+        if (Vector3.Angle(transform.up, Vector3.up) > 90f) 
+        {
+            rb.angularVelocity = Vector3.zero;
+        }        
+    }   
 }
