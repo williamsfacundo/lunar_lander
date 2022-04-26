ee3307edc5ca5114d9a6c3cae2e9f5fc247c2b8c
williamsfacundo <facuwilli@gmail.com>
2022-04-26 10:03:02 -0300

    Added 3d rotation to the spaceship.

--- Lunar_Lander/Assets/Scripts/SpaceshipRotation.cs
+++ Lunar_Lander/Assets/Scripts/SpaceshipRotation.cs
@@ -3,50 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class SpaceshipRotation : MonoBehaviour
 {
-    [SerializeField] private KeyCode rotateRightKey = KeyCode.RightArrow;
-    [SerializeField] private KeyCode rotateLeftKey = KeyCode.LeftArrow;
+    [SerializeField] private float spaceshipRotationValue = 90f;
 
-    private Rigidbody2D rigidBody2D;
-
-    private float spaceshipRotationValue = 90f;    
-    
+    private Rigidbody rigidBody;   
 
     private void Awake()
     {
-        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody>();
     }  
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float valueOne = transform.eulerAngles.z + (spaceshipRotationValue * Time.deltaTime);
-        float valueTwo = transform.eulerAngles.z - (spaceshipRotationValue * Time.deltaTime);
-
-        if ((valueOne > -5 && valueOne < 90) || (valueOne > 270 && valueOne < 365))
-        {
-            RotateSpaceshipWhenKeyPressed(spaceshipRotationValue, rotateRightKey);
-        }        
-
-        if ((valueTwo > -5 && valueTwo < 90) || (valueTwo > 270 && valueTwo < 365))
-        {
-            RotateSpaceshipWhenKeyPressed(-spaceshipRotationValue, rotateLeftKey);
-        }       
+        RotateSpaceship();        
     }
 
-    void RotateSpaceship(float value) 
+    void RotateSpaceship() 
     {
-        rigidBody2D.MoveRotation(transform.eulerAngles.z + (value * Time.deltaTime));        
-    }
-
-    void RotateSpaceshipWhenKeyPressed(float value, KeyCode key) 
-    {
-        if (Input.GetKey(key)) 
-        {
-            RotateSpaceship(value);
-        }
+        float xRotation = Input.GetAxisRaw("Vertical") * Time.deltaTime * spaceshipRotationValue;
+        float zRotation = Input.GetAxisRaw("Horizontal") * Time.deltaTime * spaceshipRotationValue;
+        
+        rigidBody.MoveRotation(Quaternion.Euler(transform.rotation.eulerAngles.x + xRotation, 0f, transform.rotation.eulerAngles.z + zRotation));        
     }
 }
