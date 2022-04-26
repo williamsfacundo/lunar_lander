63676ab65ab1749018da1f09dbce2e158d321010
unknown <facuwilli@gmail.com>
2022-04-26 16:42:03 -0300

    Added spaceship movement.

--- Lunar_Lander/Assets/Scripts/SpaceshipMovement.cs
+++ Lunar_Lander/Assets/Scripts/SpaceshipMovement.cs
@@ -1,47 +1,31 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class SpaceshipMovement : MonoBehaviour
 {
-    [SerializeField] private Vector2 horizontalSpeed = new Vector3(4f, 0f);
-    [SerializeField] private Vector2 verticalSpeed = new Vector3(0f, -4f);
-    [SerializeField] private Vector2 fallingVelocity = new Vector3(0f, -1f);
+    [SerializeField] private Vector3 speed;
 
-    private Rigidbody2D rigidBody2D;
-
-    private const float moonGravity = 1.625f;   
-
-    private Vector2 twoDimensionsPosition = new Vector2();    
-
+    [SerializeField] private Vector3 velocity; 
+    
+    private Rigidbody rb;      
+    
     private void Awake()
     {
-        rigidBody2D = GetComponent<Rigidbody2D>();
-    }    
-
+        rb = GetComponent<Rigidbody>();
+    }
+    
     // Update is called once per frame
     private void FixedUpdate()
     {
-        MoveSpaceship();
-        SpaceshipFalling();
+        MoveSpaceship();       
     }
 
     private void MoveSpaceship()
     {
-        twoDimensionsPosition.x = transform.position.x; 
-        twoDimensionsPosition.y = transform.position.y;       
-        
-        rigidBody2D.MovePosition(twoDimensionsPosition + (horizontalSpeed + verticalSpeed * Time.deltaTime));        
-    }
-
-    private void SpaceshipFalling() 
-    {
-        verticalSpeed += fallingVelocity * moonGravity * Time.deltaTime;
-    }
-    
-    public void RestSpeed(Vector2 value) 
-    {
-        horizontalSpeed.x -= value.x;
-        verticalSpeed.y -= value.y;
-    }
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rb.AddForce(transform.up, ForceMode.Impulse);
+        }                  
+    }   
 }
