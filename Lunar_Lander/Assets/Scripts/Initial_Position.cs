d314b8253c8bfaff3eafee9c5a473e76fe254a2b
facundo <facuwilli@gmail.com>
2022-05-09 22:06:29 -0300

    Player initial position equal to initial platform center.

--- Lunar_Lander/Assets/Scripts/Initial_Position.cs
+++ Lunar_Lander/Assets/Scripts/Initial_Position.cs
@@ -1,26 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Initial_Position : MonoBehaviour
 {
-    [SerializeField] private GameObject terrain_gameObject;
-
-    Terrain terrain_data;
-
-    private void Awake()
-    {
-        terrain_data = terrain_gameObject.GetComponent<Terrain>();
-    }
+    [SerializeField] private GameObject initial_platform;   
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 newPosition = terrain_gameObject.transform.position + terrain_data.terrainData.size / 2;
-        float yPos = transform.position.y;
-
-        newPosition.y = yPos; 
-
-        transform.position = newPosition;         
+        transform.position = initial_platform.GetComponent<Renderer>().bounds.center;               
     }
 }
