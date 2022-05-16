d01275c1ab1b449e0dc61725bfe217c56e6efc14
facundo <facuwilli@gmail.com>
2022-05-16 18:17:34 -0300

    Completed scene_manager script with functions that change the scene.

--- Lunar_Lander/Assets/Scripts/Scene_Manager.cs
+++ Lunar_Lander/Assets/Scripts/Scene_Manager.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void ChangeToGameScene() 
     {
-        
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);        
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void ChangeToCreditsScene() 
     {
-        
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    public static void ChangeToMenuScene() 
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+    public static void QuitGame() 
+    {
+        Application.Quit();
     }
 }
