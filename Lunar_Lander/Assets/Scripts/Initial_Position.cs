5b79aeb29699df86fcdb0ce266a50a7aede2568a
williamsfacundo <facuwilli@gmail.com>
2022-05-17 16:43:19 -0300

    Added y offset to the player initial position.

--- Lunar_Lander/Assets/Scripts/Initial_Position.cs
+++ Lunar_Lander/Assets/Scripts/Initial_Position.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Initial_Position : MonoBehaviour
 {
-    [SerializeField] private GameObject initial_platform;   
+    [SerializeField] private GameObject initial_platform;
+
+    [SerializeField] private float yOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = initial_platform.GetComponent<Renderer>().bounds.center;               
+        transform.position = initial_platform.GetComponent<Renderer>().bounds.center + (Vector3.up * yOffset);               
     }
 }
