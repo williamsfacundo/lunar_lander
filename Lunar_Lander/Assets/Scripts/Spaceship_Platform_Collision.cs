5c7a786dda0f25a3cd6dcd49d6ef92fdf925a9fb
facundo <facuwilli@gmail.com>
2022-05-17 08:00:55 -0300

    If a player collides with normal platform too fast, restart position.

--- Lunar_Lander/Assets/Scripts/Spaceship_Platform_Collision.cs
+++ Lunar_Lander/Assets/Scripts/Spaceship_Platform_Collision.cs
@@ -8,14 +8,26 @@ public class Spaceship_Platform_Collision : MonoBehaviour
 
     [SerializeField] private float angleToResetPosition;
 
+    [SerializeField] private float maxCollisionVelocity;
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.CompareTag("Platform")) 
+        if (collision.transform.CompareTag("Initial_Platform")) 
         {
             if (Vector3.Angle(transform.up, Vector3.up) >= angleToResetPosition) //80f
             {               
                 spaceshipMovement.RestartGameObject();
             }
-        }        
-    }
+        }
+        
+        if (collision.transform.CompareTag("Platform")) 
+        {
+            Debug.Log(collision.relativeVelocity.y);
+
+            if (collision.relativeVelocity.y >= maxCollisionVelocity) 
+            {
+                spaceshipMovement.RestartGameObject();
+            }            
+        }
+    }   
 }
